4182b80ffd8a5121da788fa5083dc0495029e69b
jun <grow9818@naver.com>
2024-10-01 15:03:16 +0900

    회원가입 및 로그인 페이지

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+// import FindId from './components/allMemberLogin/FindId';
+// import FindPassWord from './components/allMemberLogin/FindPassWord';
+import Login from './components/allMemberLogin/Login';
+// import LoginJoin from './components/allMemberLogin/LoginJoin';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          모여용!!!!!
-        </a>
-      </header>
-    </div>
+    <>
+    <Login/>
+   {/* <LoginJoin/>
+   <FindId/>
+   <FindPassWord/> */}
+   </>
   );
 }
 
