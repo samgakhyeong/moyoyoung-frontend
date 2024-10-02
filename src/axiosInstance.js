c53c5c2333a36c030861c41d20a5e715c14efe13
samgak <183370871+samgak0@users.noreply.github.com>
2024-10-02 16:13:56 +0900

    refresh token 구현

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -9,7 +9,7 @@ axiosInstance.interceptors.request.use((config) => {
     const state = store.getState();
     const token = state.auth.token;
 
-    const excludedUrls = ['/auth/login'];
+    const excludedUrls = ['/auth/token'];
 
     if (excludedUrls.some(url => config.url.includes(url))) {
         return config;
@@ -22,6 +22,33 @@ axiosInstance.interceptors.request.use((config) => {
     }
     return config;
 }, (error) => {
+    return Promise.reject(error);
+});
+
+axiosInstance.interceptors.response.use((response) => {
+    return response;
+}, async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+
+        const state = store.getState();
+        const refreshToken = state.auth.refreshToken;
+
+        try {
+            const response = await axios.post('http://localhost:8080/auth/refresh', { token: refreshToken });
+            const { token } = response.data;
+
+            store.dispatch({ type: 'SET_TOKEN', payload: token });
+            originalRequest.headers['Authorization'] = `Bearer ${token}`;
+
+            return axiosInstance(originalRequest);
+        } catch (refreshError) {
+            return Promise.reject(new Error('토큰 갱신 실패.'));
+        }
+    }
+
     return Promise.reject(error);
 });
 
